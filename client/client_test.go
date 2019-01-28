dd808250d56cf8d9ef479c0e3c9bd62185c6b555
jiangruocheng <ruocheng.jiang@tinklabs.com>
2019-01-28 19:17:49 +0800

    update

--- client/client_test.go
+++ client/client_test.go
@@ -1,9 +1,6 @@
 package client
 
 import (
-	"fmt"
-	"testing"
-
 	"github.com/tinklabs/golibs/cmd"
 	"github.com/tinklabs/golibs/consul"
 	"github.com/tinklabs/golibs/log"
@@ -13,11 +10,4 @@ func init() {
 	cmd.Init()
 	log.Init()
 	consul.Init()
-	Init()
-}
-
-func TestGetService(t *testing.T) {
-	s, ok := GetService("b2c-gateway")
-	fmt.Printf("s = %+v\n", s)
-	fmt.Printf("ok = %+v\n", ok)
 }
